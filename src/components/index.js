be2d7fb7c2e5216e3e370eb0cbf599cc5751baf6
Dmitry Shagurov <shagurovda@gmail.com>
2022-03-03 23:06:26 +0300

    QuizThirdStep component added; styles for QuizThirdStep added

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { default as ModalWindow } from "./quiz/ModalWindow";
 
 export { default as QuizFirstStep } from "./quiz/steps/QuizFirstStep";
 export { default as QuizSecondStep } from "./quiz/steps/QuizSecondStep";
+export { default as QuizThirdStep } from "./quiz/steps/QuizThirdStep";
 
 
 export { default as Button } from "./UI/Button";
---
+++ src/components/quiz/steps/QuizThirdStep.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ModalWindow from "../ModalWindow";
+import Button from "../../UI/Button";
+import "../../../styles/scss/components/quiz/QuizThirdStep.scss";
+
+
+function QuizThirdStep() {
+  return (
+    <ModalWindow className="quizThird-step">
+      <h1 className="modal-window__label quiz-third-step__label">Шаг 3</h1>
+      <p className="modal-window__massage quiz-third-step__massage">
+        Вы не будете оскорблять или писать негативного девушкам,
+        членам нашего клуба, вы согласны?
+      </p>
+      <div className="quiz-third-step__buttons-wrapper">
+        <Button
+          className="quiz-third-step__button"
+          theme="main"
+          children="Да"
+        />
+        <Button
+          className="quiz-third-step__button"
+          theme="additional"
+          children="Нет"
+        />
+      </div>
+    </ModalWindow>
+  )
+}
+
+
+export default QuizThirdStep;
