8a39e8fa9ce70c3dbd53e95dc452b17e12c40a92
Dmitry Shagurov <shagurovda@gmail.com>
2022-03-03 22:28:22 +0300

    classnames and prop-types integration for Button component

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import "../../styles/scss/components/UI/Button.scss";
 
 
-function Button({ children }) {
+function Button({ className, theme, onClick, children }) {
   return (
-    <button className="Button">{ children }</button>
+    <button
+      onClick={onClick}
+      className={classNames(
+        "Button",
+        className,
+        { [`Button_theme_${theme}`]: theme }
+      )}
+    >
+      {children}
+    </button >
   )
 }
+
+Button.propTypes = {
+  className: PropTypes.string,
+  theme: PropTypes.string,
+  onClick: PropTypes.func,
+  children: PropTypes.any
+};
 
 
 export default Button;
