fe4d352327ab9669fddef9cff4af2f5074ebc448
Dmitry Shagurov <shagurovda@gmail.com>
2022-03-03 22:24:05 +0300

    classnames and prop-types integration for ModalWindow component

--- src/components/quiz/ModalWindow.jsx
+++ src/components/quiz/ModalWindow.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
 import "../../styles/scss/components/quiz/ModalWindow.scss";
 
 
-function ModalWindow({ children }) {
+function ModalWindow({ className, children }) {
   return (
-    <div className="modal-window">
+    <div className={classNames("modal-window", className)}>
       <div className="modal-window__back" />
       <div className='modal-window__window'>{ children }</div>
     </div>
   )
 }
 
+ModalWindow.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.any
+};
 
 export default ModalWindow;
