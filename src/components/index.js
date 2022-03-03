0595dabb5cee71d2e0b731a9493f0205cff2f43d
Dmitry Shagurov <shagurovda@gmail.com>
2022-03-03 23:14:14 +0300

    QuizFourthStep component added; styles for QuizFourthStep added

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as ModalWindow } from "./quiz/ModalWindow";
 export { default as QuizFirstStep } from "./quiz/steps/QuizFirstStep";
 export { default as QuizSecondStep } from "./quiz/steps/QuizSecondStep";
 export { default as QuizThirdStep } from "./quiz/steps/QuizThirdStep";
+export { default as QuizFourthStep } from "./quiz/steps/QuizFourthStep";
 
 
 export { default as Button } from "./UI/Button";
---
+++ src/components/quiz/steps/QuizFourthStep.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ModalWindow from "../ModalWindow";
+import Button from "../../UI/Button";
+import "../../../styles/scss/components/quiz/QuizFourthStep.scss";
+
+
+function QuizFourthStep() {
+  return (
+    <ModalWindow className="quiz-fourth-step">
+      <h1 className="modal-window__label quiz-fourth-step__label">Шаг 4</h1>
+      <p className="modal-window__massage quiz-fourth-step__massage">
+        Вы несете ответсвенность за информацию,
+        которую размещаете на нашем сайте?
+      </p>
+      <div className="quiz-fourth-step__buttons-wrapper">
+        <Button
+          className="quiz-fourth-step__button"
+          theme="main"
+          children="Да"
+        />
+        <Button
+          className="quiz-fourth-step__button"
+          theme="additional"
+          children="Нет"
+        />
+      </div>
+    </ModalWindow>
+  )
+}
+
+
+export default QuizFourthStep;
