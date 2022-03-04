8e6c2d100d863133362b66a73dcf3e3ed09361c4
Dmitry Shagurov <shagurovda@gmail.com>
2022-03-04 11:29:22 +0300

    QuizForm component added

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ export { default as QuizSecondStep } from "./quiz/steps/QuizSecondStep";
 export { default as QuizThirdStep } from "./quiz/steps/QuizThirdStep";
 export { default as QuizFourthStep } from "./quiz/steps/QuizFourthStep";
 export { default as QuizSuccess } from "./quiz/steps/QuizSuccess";
+export { default as QuizForm } from "./quiz/steps/QuizForm";
 
 
 export { default as Button } from "./UI/Button";
---
+++ src/components/quiz/steps/QuizForm.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ModalWindow from "../ModalWindow";
+
+
+function QuizForm() {
+  return (
+    <ModalWindow className="quiz-form">
+      <h1 className="modal-window__label quiz-form__label">
+        Заполните форму:
+      </h1>
+      
+    </ModalWindow>
+  )
+}
+
+
+export default QuizForm;
