ba586a589608f24e7689c4506b27c29a430a2c65
Dmitry Shagurov <shagurovda@gmail.com>
2022-03-05 19:47:41 +0300

    Button component a-tag support added

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -5,29 +5,48 @@ import "../../styles/scss/components/UI/Button.scss";
 
 
 function Button(
-  { className, theme, disabled, type, onClick, children }
+  { className, theme, disabled, type, onClick, link, href, children }
 ) {
-  return (
-    <button
-      onClick={onClick}
-      className={classNames(
-        "Button",
-        className,
-        { [`Button_theme_${theme}`]: theme }
-      )}
-      type={type ? type : "button"}
-      disabled={disabled}
-    >
-      {children}
-    </button >
-  )
+  if (link) {
+    return (
+      <a
+        onClick={onClick}
+        className={classNames(
+          "Button",
+          className,
+          { [`Button_theme_${theme}`]: theme }
+        )}
+        href={href}
+      >
+        {children}
+      </a>
+    )
+  } else {
+    return (
+      <button
+        onClick={onClick}
+        className={classNames(
+          "Button",
+          className,
+          { [`Button_theme_${theme}`]: theme }
+        )}
+        type={type ? type : "button"}
+        disabled={disabled}
+      >
+        {children}
+      </button>
+    )
+  }
 }
+
 
 Button.propTypes = {
   className: PropTypes.string,
   theme: PropTypes.string,
   disabled: PropTypes.bool,
   type: PropTypes.string,
+  link: PropTypes.bool,
+  href: PropTypes.string,
   onClick: PropTypes.func,
   children: PropTypes.any
 };
